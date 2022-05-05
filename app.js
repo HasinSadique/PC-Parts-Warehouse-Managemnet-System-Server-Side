9ede6e0fc7fc0c17c79242615d2e8d0072a4fad3
hasinsadique <hasinsadique7@gmail.com>
2022-05-06 01:43:11 +0600

    Single item  Api complete

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 app.use(cors());
 app.use(express.json({ extended: false }));
 
 const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server started on port: ", port);
+});
 
 // username: dbuser1
 // pass: zSTx5E7lwH9ZoA7m
@@ -29,9 +33,19 @@ async function run() {
       .collection("Items");
 
     app.post("/add-item", async (req, res) => {
-      inventoryItemsCollection.findOne(req.body.itemName);
-      const id = (await inventoryItemsCollection.insertOne(req.body))
-        .insertedId;
+      const { imageUrl, itemName, supplierName, itemPrice, description } =
+        req.body;
+      let item = {
+        imageUrl: imageUrl,
+        itemName: itemName,
+        supplierName: supplierName,
+        itemPrice: itemPrice,
+        description: description,
+        quantity: 0,
+      };
+      console.log(req.body);
+      // inventoryItemsCollection.findOne(req.body.itemName);
+      const id = (await inventoryItemsCollection.insertOne(item)).insertedId;
       res.send({ response: `Item Inserted with ID: ${id} ` });
     });
 
@@ -41,6 +55,14 @@ async function run() {
       const items = await cursor.toArray();
       res.send(items);
     });
+
+    app.get("/getItems/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log("id>>>: ", id);
+      const query = { _id: ObjectId(id) };
+      const item = await inventoryItemsCollection.findOne(query);
+      res.send(item);
+    });
   } catch (e) {
     console.log("error is: ", e);
   } finally {
@@ -48,16 +70,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port, () => {
-  console.log("Server started on port: ", port);
-});
-
 app.get("/", (req, res) => {
   res.send("Hello User Welcome to Warehouse Management Site Server");
 });
-
-// app.post("/add-items", async (req, res) => {
-//   const { imageUrl, itemName, supplierName, itemPrice, description } = req.body;
-//   console.log(description);
-//   // console.log(imageUrl);
-// });
